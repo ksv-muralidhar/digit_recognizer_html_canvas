520525eddcc4e1202bbd49faaf8072eedab87bf5
KSV Muralidhar <murali_dhar0552@yahoo.com>
2022-06-18 20:50:30 +0530

    Update mnist.py

--- src/mnist.py
+++ src/mnist.py
@@ -16,9 +16,12 @@ y_train = y_train[shuffle_idx]
 x_train = x_train.reshape(len(x_train), 28, 28, 1)
 x_test = x_test.reshape(len(x_test), 28, 28, 1)
 
-train_datagen = ImageDataGenerator(
-                                    rescale=1. / 255,
-                                    fill_mode='nearest')
+train_datagen = ImageDataGenerator(height_shift_range=0.1,
+                                   width_shift_range=0.25,
+                                   rotation_range=30,
+                                   zoom_range=0.2,
+                                   rescale=1. / 255,
+                                   fill_mode='nearest')
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 tf.random.set_seed(11)
 cnn = tf.keras.models.Sequential()
@@ -33,16 +36,17 @@ cnn.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1,
                                activation='relu', padding="same"))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 cnn.add(tf.keras.layers.Flatten())
-cnn.add(tf.keras.layers.Dense(256, activation="relu"))
+cnn.add(tf.keras.layers.Dense(128, activation="relu"))
 cnn.add(tf.keras.layers.Dropout(0.2))
-cnn.add(tf.keras.layers.Dense(256, activation="relu"))
+cnn.add(tf.keras.layers.Dense(128, activation="relu"))
 cnn.add(tf.keras.layers.Dropout(0.2))
 cnn.add(tf.keras.layers.Dense(10, activation="softmax"))
 cnn.compile(optimizer="adam", loss="sparse_categorical_crossentropy",
             metrics="sparse_categorical_accuracy")
-early_stop = EarlyStopping(monitor='val_loss', patience=3)
+early_stop = EarlyStopping(monitor='val_loss', patience=5)
 training_data = train_datagen.flow(x_train, y_train, batch_size=32, seed=42)
 validation_data = test_datagen.flow(x_test, y_test, batch_size=32, seed=42, shuffle=False)
 history = cnn.fit(training_data, epochs=500, callbacks=[early_stop],
                   validation_data=validation_data)
+
 cnn.save(os.path.join('models', 'mnist.h5'))
